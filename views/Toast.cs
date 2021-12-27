47b89a03764c23256a51ed8dd716b4f12a7745e9
AkilanThamarai <akilan.hashmicro@gmail.com>
2021-12-27 12:35:06 +0530

    Inital commit

---
+++ views/Toast.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SalesApp.views
+{
+    public class Toast : ContentPage
+    {
+        public Toast()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
